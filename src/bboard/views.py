09f2cb7a9501aea6a13520f496045689b7f48c35
luta-wolf <denis.skrynnikov.job@gmail.com>
2023-01-05 20:41:00 +0300

    1.12 Template rendering

--- src/bboard/views.py
+++ src/bboard/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+from . models import Bb
 
 # Create your views here.
+# def index(request):
+# 	s = 'Список объявлений\r\n\r\n\r\n'
+# 	for bb in Bb.objects.order_by('-published'):
+# 		s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+# 	return HttpResponse(s, content_type='text/plan; charset=utf-8')
+
+
+# def index(request):
+# 	template = loader.get_template('bboard/index.html')
+# 	bbs = Bb.objects.order_by('-published')
+# 	context = {'bbs': bbs}
+# 	return HttpResponse(template.render(context, request))
+
+
 def index(request):
-	return HttpResponse("Здесь будет список объявлений")
+	bbs = Bb.objects.order_by('-published')
+	return render(request, 'bboard/index.html', {'bbs': bbs})
