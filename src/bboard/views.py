f567478b6a577b2524d68a9d0e4a1c530c91fabe
luta-wolf <denis.skrynnikov.job@gmail.com>
2023-01-09 20:57:56 +0300

    add statuc file

--- src/bboard/views.py
+++ src/bboard/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
 from . models import Bb, Rubric
@@ -24,7 +25,8 @@ def by_rubric(request, rubric_id):
 class BbCreateView(CreateView):
 	template_name = 'bboard/create.html'
 	form_class = BbForm
-	success_url = '/bboard/'
+	# success_url = '/bboard/'
+	success_url = reverse_lazy('index')
 
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
