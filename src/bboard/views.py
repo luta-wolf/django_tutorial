8984079a22c32b3eb4d5ce39bdebcc6bf7938e97
luta-wolf <denis.skrynnikov.job@gmail.com>
2023-01-09 17:25:40 +0300

    added adding ads

--- src/bboard/views.py
+++ src/bboard/views.py
@@ -1,22 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 
 from . models import Bb, Rubric
-
-# Create your views here.
-# def index(request):
-# 	s = 'Список объявлений\r\n\r\n\r\n'
-# 	for bb in Bb.objects.order_by('-published'):
-# 		s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-# 	return HttpResponse(s, content_type='text/plan; charset=utf-8')
-
-
-# def index(request):
-# 	template = loader.get_template('bboard/index.html')
-# 	bbs = Bb.objects.order_by('-published')
-# 	context = {'bbs': bbs}
-# 	return HttpResponse(template.render(context, request))
+from .forms import BbForm
 
 
 def index(request):
@@ -32,3 +20,13 @@ def by_rubric(request, rubric_id):
 	current_rubric = Rubric.objects.get(pk=rubric_id)
 	context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
 	return render(request, 'bboard/by_rubric.html', context)
+
+class BbCreateView(CreateView):
+	template_name = 'bboard/create.html'
+	form_class = BbForm
+	success_url = '/bboard/'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['rubrics'] = Rubric.objects.all()
+		return context
